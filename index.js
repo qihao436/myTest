fadf65f3a02e37e68ebf045c98bb6ab008312ab8
qihao436 <qihao436@hotmail.com>
2017-06-21 14:15:47 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
 var http = require('http');
 var fs = require('fs');
-http.createServer(function(res,req){
-  switch (res.url) {
-    case '/graph.json':
-      req.end(fs.readFileSync('graph.json'));
-      break;
-    case '/d3.v4.min.js':
-    req.end(fs.readFileSync('d3.v4.min.js'));
-    break;
+http.createServer(function(res, req) {
+  console.log(res.url);
 
-    default:
+    try {
+      req.end(fs.readFileSync(res.url.substring(1)));
+    } catch (e){
       req.end(fs.readFileSync('index.html'));
-  }
+    }
+
 }).listen(8080);
